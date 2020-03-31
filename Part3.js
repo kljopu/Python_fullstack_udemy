3aa453ed4b73f13c5a11a2b436461829b7ea0cc4
kyleHan <kljopuu@gmail.com>
2020-04-01 00:21:07 +0900

    4 - 01

---
+++ Part3.js
@@ -0,0 +1,3 @@
+var pound= prompt("Enter amount of pound you want to convert: ");
+var kilogram = alert(pound+"pound is " +pound*0.454+ "kilogram");
+console.log("Conversion Completed");
---
+++ myscript.js
@@ -0,0 +1,4 @@
+alert("Welcome to your Bank!");
+var deposit = prompt("How much would you like to deposit today?");
+alert("You've doposited: "+deposit);
+console.log("You are a cool person!");
