8f48fb61297347de936eb137688c208dc7c349c0
Kyle Han <kljopuu@gmail.com>
2020-04-12 19:31:49 +0900

    PC upload

---
+++ JavaScript_Level_One/myscript2.js
@@ -0,0 +1,13 @@
+var ham = 10;
+var cheese =10;
+
+var report = "blanck";
+if (ham >= 10 && cheese >=10) {
+    report = "Strong sales of both ham and cheese!"
+    
+}else if(ham === 0 && cheese ===0){
+    report = "Nothing Sold!"
+}else{
+    roport = "We had some sales of "
+}
+console.log(report);
